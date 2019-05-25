b098b046899fd1a5d7493206665d985ea79e785c
SahilShah518 <sahilshah379@gmail.com>
2019-05-24 21:20:28 -0400

    lmao

--- Hydroponics.ino
+++ Hydroponics.ino
@@ -1,67 +1,38 @@
 #include <FastLED.h>
 
 #define LED_PIN     5
-#define NUM_LEDS    10
+#define NUM_LEDS    30
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 
 CRGB leds[NUM_LEDS];
+CRGB color;
+unsigned long startTime;
+boolean on;
+long intervalOn = 14*3600*1000;
+long intervalOff = 10*3600*1000;
 
 void setup() {
     delay(3000);
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    color = CRGB(255, 0, 255);
+    on = true;
+    startTime = millis();
 }
 void loop() {
     for (int x = 0; x < NUM_LEDS; x++) {
-        leds[x] = CRGB(255, 0, 255);
+        leds[x] = color;
     }
     FastLED.show();
-    delay(10);
-}
 
-//#define DHpin 8
-//byte dat [5];
-//
-//void setup () {
-//  Serial.begin (9600);
-//  pinMode (DHpin, OUTPUT);
-//}
-// 
-//void loop () {
-//    digitalWrite (DHpin, LOW);
-//    delay (30);
-//    digitalWrite (DHpin, HIGH);
-//    delayMicroseconds (40);
-//    pinMode (DHpin, INPUT);
-//    while (digitalRead (DHpin) == HIGH);
-//    delayMicroseconds (80);
-//    if (digitalRead (DHpin) == LOW);
-//    delayMicroseconds (80);
-//    for (int i = 0; i < 4; i ++) {
-//        byte data;
-//        for (int i = 0; i < 8; i ++) {
-//            if (digitalRead (DHpin) == LOW) {
-//                while (digitalRead (DHpin) == LOW);
-//                delayMicroseconds (30);
-//                if (digitalRead (DHpin) == HIGH){
-//                    dat[i] |= (1 << (7-i));
-//                }
-//                while (digitalRead (DHpin) == HIGH);
-//            }
-//        }
-//    }
-//    pinMode (DHpin, OUTPUT);
-//    digitalWrite (DHpin, HIGH);
-//    
-//    Serial.print ("Current humdity =");
-//    Serial.print (dat [0], DEC);
-//    Serial.print ('.');
-//    Serial.print (dat [1], DEC);
-//    Serial.println ('%');
-//    Serial.print ("Current temperature =");
-//    Serial.print (dat [2], DEC);
-//    Serial.print ('.');
-//    Serial.print (dat [3], DEC);
-//    Serial.println ('C');
-//    delay (700);
-//}
+    unsigned long currentTime = millis();
+    if (on && (currentTime - startTime >= intervalOn)) {
+        color = CRGB(0, 0, 0);
+        on = false;
+        startTime = millis();
+    } else if (!on && (currentTime - startTime >= intervalOff)) {
+        color = CRGB(255, 0, 255);
+        on = true;
+        startTime = millis();
+    }
+}
